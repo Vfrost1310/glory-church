aaedc32c7713167de5905c67a868551cdd9cfee3
Vadym <vadym131003@gmail.com>
2025-11-05 11:26:23 +0100

    cpanel init

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>Glory Church</title>
     <link rel="icon" href="./img/favicon.ico" type="image/x-icon" />
     <link rel="stylesheet" href="/css/all.min.css" />
-    <link rel="stylesheet" href="docs/css/style.css" />
+    <link rel="stylesheet" href="/css/style.css" />
     <link rel="stylesheet" href="/css/home.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -32,86 +32,7 @@
   </head>
   <body>
     <header>
-      <div id="head-cont" class="container head-cont">
-        <div class="logo">
-          <a href="/">
-            <img
-              loading="lazy"
-              src="/img/logo.png"
-              alt="Logo Chiesa"
-              height="72"
-            />
-          </a>
-        </div>
-
-        <nav class="navbar">
-          <ul class="nav-menu">
-            <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
-            <li class="nav-item">
-              <a class="nav-link" href="/pages/about.html">Su Noi</a>
-            </li>
-            <li class="dropdown">
-              <a class="nav-link" href="/pages/resources/resources.html"
-                >Risorse Online</a
-              >
-              <ul class="dropmenu">
-                <li>
-                  <a class="nav-link" href="/pages/resources/prop.html"
-                    >Sermoni</a
-                  >
-                </li>
-                <li>
-                  <a class="nav-link" href="/pages/resources/music.html"
-                    >Musica</a
-                  >
-                </li>
-                <li>
-                  <a class="nav-link" href="/pages/resources/books.html"
-                    >Libri</a
-                  >
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/pages/activity.html">Eventi</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/pages/contacts.html">Contatti</a>
-            </li>
-          </ul>
-        </nav>
-
-        <div class="head-tools">
-          <div class="dropdown">
-            <a class="nav-link" href="#" class="current-lang"
-              ><img
-                loading="lazy"
-                src="/img/icons/italy.webp"
-                alt="Ukrainian flag icon"
-            /></a>
-            <ul class="dropmenu">
-              <li>
-                <a href="en/index.html"
-                  ><img
-                    loading="lazy"
-                    src="/img/icons/ukraine.webp"
-                    alt="English flag icon"
-                /></a>
-              </li>
-              <li>
-                <a href="ru/index.html"
-                  ><img
-                    loading="lazy"
-                    src="/img/icons/uk.webp"
-                    alt="Russian flag icon"
-                /></a>
-              </li>
-            </ul>
-          </div>
-
-          <button class="menu-btn" aria-label="Apri Menu">&#9776;</button>
-        </div>
-      </div>
+      <?php include './pages/common/header.php'; ?>
     </header>
     <main>
       <section id="hero-section" class="hero">
---
+++ pages/common/header.php
@@ -0,0 +1,65 @@
+<div id="head-cont" class="container head-cont">
+    <div class="logo">
+        <a href="/">
+            <img
+                loading="lazy"
+                src="/img/logo.png"
+                alt="Logo Chiesa"
+                height="72" />
+        </a>
+    </div>
+
+    <nav class="navbar">
+        <ul class="nav-menu">
+            <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
+            <li class="nav-item">
+                <a class="nav-link" href="/pages/about.html">Su Noi</a>
+            </li>
+            <li class="dropdown">
+                <a class="nav-link" href="/pages/resources/resources.html">Risorse Online</a>
+                <ul class="dropmenu">
+                    <li>
+                        <a class="nav-link" href="/pages/resources/prop.html">Sermoni</a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="/pages/resources/music.html">Musica</a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="/pages/resources/books.html">Libri</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/pages/activity.html">Eventi</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/pages/contacts.html">Contatti</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="head-tools">
+        <div class="dropdown">
+            <a class="nav-link" href="#" class="current-lang"><img
+                    loading="lazy"
+                    src="/img/icons/italy.webp"
+                    alt="Ukrainian flag icon" /></a>
+            <ul class="dropmenu">
+                <li>
+                    <a href="en/index.html"><img
+                            loading="lazy"
+                            src="/img/icons/ukraine.webp"
+                            alt="English flag icon" /></a>
+                </li>
+                <li>
+                    <a href="ru/index.html"><img
+                            loading="lazy"
+                            src="/img/icons/uk.webp"
+                            alt="Russian flag icon" /></a>
+                </li>
+            </ul>
+        </div>
+
+        <button class="menu-btn" aria-label="Apri Menu">&#9776;</button>
+    </div>
+</div>
